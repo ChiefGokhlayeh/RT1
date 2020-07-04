152eb7628b8f412c86576aa85d2718ba789c070a
Andreas Baulig <free.geronimo@hotmail.de>
2020-07-04 19:41:32 +0200

    Complete lab 1 exercises

---
+++ lab1/SpringenderBall_ex7.m
@@ -0,0 +1,39 @@
+% Laborversuch: Simulation
+% Regelungstechnik 1
+% (C) 2019 W.Lindermeir, W.Zimmermann
+% Hochschule Esslingen
+%
+
+slCharacterEncoding('UTF-8')
+clear all		% Alle Variablen löschen
+
+g=9.81;			% Erdbeschleunigung
+T=0.04;			% Simulationsschrittweite
+Tend = 10;		% Simulationsdauer
+
+S=0.8;			% Stoßfaktor plastisch=0 ... 1=elastisch
+
+t=0;			% Zeit
+y=0;			% Höhe (Anfangshöhe)
+vy=8;			% Geschwindigkeit in y-Richtung
+vx=0.2;			% Geschwindigkeit in x-Richtung
+x=0;			% Horizontale Position
+
+SpringenderBallAnimation(t, x, y, 1, Tend, 1, 5);	% Zeitdiagramm und Animation initialisieren
+
+disp('Verschieben Sie die Fenster so, dass beide Grafikfenster sichtbar sind');
+disp('Drücken Sie die Leertaste, um die Simulation zu starten');
+%pause			% Warten auf Tastendruck
+
+for t = 0 : T : Tend	% Simulationsschleife
+    vy = vy - g * T;	% --- Geschwindigkeit
+    y  =  y + T * vy;	% --- Höhe
+    x  =  x + T * vx;   % --- Horizontale Position
+    
+    if y <= 0	        % Falls Boden erreicht
+    	vy = -vy * S;	% --- Teilelastischer Stoß
+    	y =  0;		    % --- Höhe
+        vx = S*vx;      % --- Horizontale Geschwindigkeit verringern
+    end
+    SpringenderBallAnimation(t, x, y, 0);    		% Zeitdiagramm und Animation zeichnen
+end
